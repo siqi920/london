bf1f8334f645d9146f43f85245d53aa9024fee77
Rossella Ferrandino <39148794+RossellaFer@users.noreply.github.com>
2023-02-18 16:10:03 +0100

    feat: Reorganize menu with submenu(#58)
    - add new menu and sub-menu structure in navbar.yml 
    - add logic to display dropdown if item has subfolder items in
    navbar.html
    - adjust custom styles to fit the length of the link names
    - adjust JS logic to highlight the current link to work with link names
    that don't correspond to the handleized url (e.g. Long-term relationship
    timeline -> timeline)

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,21 +1,26 @@
 var controllerNavbar = (function (jQuery) {
   var init = function () {
     // Get the first element of the pathname and transform
-    // e.g /london/code-of-conduct => "code of conduct"
     var pathSegments = jQuery(location).attr('pathname').split("/");
-    var firstPathSegment = pathSegments[pathSegments.length - 1].replaceAll("-", " ");
+    //transform to get links with text corresponding to url
+    // e.g /london/code-of-conduct => "code of conduct"
+    var firstPathSegment = pathSegments[pathSegments.length - 1];
+    var stringPathSegment = firstPathSegment.replaceAll("-", " ");
 
     // Looks up for the link to mark as current link
     var activeLink;
-    if (!firstPathSegment) {
+    var activeSubLink;
+    if (!stringPathSegment) {
       activeLink = jQuery("a.nav-link:contains('Home')");
     } else {
       // Format the text as provided in data, e.g code of conduct => "Code of conduct"
-      var searchLinkText = firstPathSegment[0].toUpperCase() + firstPathSegment.slice(1, firstPathSegment.length);
+      var searchLinkText = stringPathSegment[0].toUpperCase() + stringPathSegment.slice(1, stringPathSegment.length);
       activeLink = jQuery("a.nav-link:contains('" + searchLinkText + "')");
+      activeSubLink = jQuery("a.nav-link[href='" + firstPathSegment + "']")
     }
 
     activeLink.addClass("active-nav");
+    activeSubLink.addClass("active-nav");
   };
 
   return {
